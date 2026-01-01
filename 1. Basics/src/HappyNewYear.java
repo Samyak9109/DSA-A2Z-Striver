297201fb41cb082f44fff31cebc31d4b00bb873a
Samyak Jain <samyak.9109@gmail.com>
2026-01-01 18:39:11 +0530

    Happy new year 2026

---
+++ 1. Basics/src/HappyNewYear.java
@@ -0,0 +1,7 @@
+public class HappyNewYear {
+    public static void main(String[] args) {
+        for (int i = 2024; i <= 2124; i++) {
+            System.out.println("Happy New Year " + i + "!");
+        }
+    }
+}
